420313b00bb20af4f4f4f56c64a2f50bf1d320bc
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-18 09:13:51 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // - Läs in X temperaturangivelser från en fil
+        // - Spara i en array
+        // - Räkna ut medelvärdet
+        // - Skriv ut alla temperaturer (+ rad i filen) som är högre än medelvärdet
+
+
+    }
+}
